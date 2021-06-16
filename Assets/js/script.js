43f651a6d4a9216ade4057ad4b1420e9b151c7c5
Shilo <rachel.brain@internode.on.net>
2021-06-17 07:48:53 +1000

    3rd commit with updated javascript files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,28 +1,118 @@
 var timeEl = document.querySelector(".timer-box");
 var startBtn = document.getElementById("start-button");
-// var subtractTime= document.querySelector("subtract-time");
 var questionEl = document.querySelector(".question");
-var optionsBtns = document.querySelector(".options-button");
+var optionsBtns = document.querySelectorAll("button.option");
 var rulesCardEl = document.querySelector(".rules-card");
 var questionsCardEl = document.querySelector(".questions-card");
+var loserCardEl = document.querySelector(".loser-card");
 var choiceResponseEl = document.querySelector("#response");
+
+var optionOneEl = document.querySelector(".options-button-one");
+var optionTwoEl = document.querySelector(".options-button-two");
+var optionThreeEl = document.querySelector(".options-button-three");
+var optionFourEl = document.querySelector(".options-button-four");
 
 
 // Order:
-// 1. Display the Rules of the game on load with the Start Button front & centre.
-// 2. The Timer - with a start time of 75 seconds - is displayed on the top right of the page.
-// 3. Once the user presses the Start button, the rules disappear & the first Question (chosen randomly) appears with 4 answer Options & the Timer begins its countdown.
-// 4. a) If the user chooses the correct option, they are given a message (word/color/sound) that they are correct.
-//    b) If the user chooses a wrong option, they are given a message (word/color/sound) that they are incorrect & 10 seconds will be removed from the time remaining.
-// 5. Then, the next Question (chosen randomly) appears with 4 answer Options & steps 4 & 5 are repeated.
+// working - 1. Display the Rules of the game on load with the Start Button front & centre.
+// working - 2. The Timer - with a start time of 75 seconds - is displayed on the top right of the page.
+// working - 3. Once the user presses the Start button, the rules disappear & the first Question (chosen randomly) appears with 4 answer Options & the Timer begins its countdown.
+// working - 4. a) If the user chooses the correct option, they are given a message (word/color/sound) that they are correct.
+// working - 4. b) If the user chooses a wrong option, they are given a message (word/color/sound) that they are incorrect.
+// working - 4. c) If the choice is incorrect, 10 seconds will be removed from the time remaining.
+// working - 5. Then, the next Question (chosen randomly) appears with 4 answer Options & steps 4 & 5 are repeated.
 // 6. a) The Game ends either when there are no questions remaining & the user is told his/her score & is asked if they would like to record their score on the high scores page.
-//    b) Or the Game ends when the Timer reaches zero & the user is told they have failed/lost/time is up. 
+// working - 6. b) Or the Game ends when the Timer reaches zero & the user is told they have failed/lost/time is up. 
 // 7. Local storage will be needed to record the high scores on the high scores page & have them persist.
 
 // RESET TIMER TO 75 SECONDS!!!!!
+// REMOVE console logs
 
 
-var time = 4;
+var quizQuestions = [{
+        question: "Arrays in JavaScript can be used to store?",
+        option1: "Numbers & Strings",
+        option2: "Booleans",
+        option3: "Other arrays",
+        option4: "All of the above",
+        correctAnswer: 4
+    },
+    {
+        question: "Commonly used data types DO NOT include?",
+        option1: "Strings",
+        option2: "Booleans",
+        option3: "Alerts",
+        option4: "Numbers",
+        correctAnswer: 3
+    },
+    {
+        question: "String values must be enclosed in?",
+        option1: "Single or double quotation marks",
+        option2: "Curly brackets",
+        option3: "Parenthesis",
+        option4: "Square brackets",
+        correctAnswer: 1
+    },
+    {
+        question: "Which of the following methods removes the last element from an array and returns that element?",
+        option1: "get()",
+        option2: "last()",
+        option3: "pop()",
+        option4: "None of the above",
+        correctAnswer: 3
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        option1: "*",
+        option2: "X",
+        option3: "==",
+        option4: "=",
+        correctAnswer: 4
+    },
+    {
+        question: "Which of these is NOT a JavaScript comparison operator?",
+        option1: "===",
+        option2: "==!",
+        option3: ">=",
+        option4: "?",
+        correctAnswer: 2
+    },
+    {
+        question: "JavaScript uses the + operator for both addition and concatenation.  Which of the following statements is TRUE?",
+        option1: "Numbers are concatenated. Strings are added.",
+        option2: "Numbers are added. Strings are concatenated.",
+        option3: "Both Numbers and Strings can be concatenated.",
+        option4: "Both Numbers and Strings can be added.",
+        correctAnswer: 2
+    },
+    {
+        question: "Is the console a useful tool used in debugging of JavaScript?",
+        option1: "Yes, always",
+        option2: "More often than not",
+        option3: "Rarely",
+        option4: "Never",
+        correctAnswer: 1
+    },
+    {
+        question: "Which of the following String functions creates a string and displays it in a large font as if it were in a tag?",
+        option1: "anchor()",
+        option2: "blink()",
+        option3: "strong()",
+        option4: "big()",
+        correctAnswer: 4
+    },
+    {
+        question: "How do you write an IF statement in JavaScript?",
+        option1: "if (i==5)",
+        option2: "if i=5 then",
+        option3: "if i==5 then",
+        option4: "if i=5",
+        correctAnswer: 1
+    }
+];
+
+
+var time = 25;
 var interval = null;
 
 var startTimer = function () {
@@ -30,32 +120,17 @@ var startTimer = function () {
         if (time > 0) {
             time--;
             timeEl.textContent = time;
-            console.log(time);
         } else if (time === 0) {
-            // loseGame();
+            loseGame();
             clearInterval(interval);
-            console.log("Time's up!");
             timeEl.textContent = "Time's up!";
         }
     }, 1000);
     startQuiz();
 };
 
-
-var quizQuestions;
-
-// var question;
-// var options;// var correctAnswer;
-
-// var optionsSplit = quizQuestions.options.split(",");
-
-var optionsBtnOne = document.querySelector(".one");
-var optionsBtnTwo = document.querySelector(".two");
-var optionsBtnThree = document.querySelector(".three");
-var optionsBtnFour = document.querySelector(".four");
-
-
 var shuffledQuestions;
+var currentQuestion;
 var currentQuestionIndex;
 var response;
 
@@ -68,109 +143,84 @@ function startQuiz() {
 }
 
 function setNextQuestion() {
+    choiceResponseEl.classList.add("hide");
+    console.log(shuffledQuestions);
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(quizQuestions) {
+    var answer = quizQuestions.correctAnswer;
+    var userInput;
     questionEl.innerText = quizQuestions.question;
-    quizQuestions.options.forEach(answer => {
-        // optionsBtns[0].innerText = quizQuestions.options[0];
-        // optionsBtns[1].innerText = quizQuestions.options[1];
-        // optionsBtns[2].innerText = quizQuestions.options[2];
-        // optionsBtns[3].innerText = quizQuestions.options[3];
-        // optionsBtns.one = quizQuestions.options[0].text;
-        // optionsBtns.two = quizQuestions.options[1].text;
-        // optionsBtns.three = quizQuestions.options[2].text;
-        // optionsBtns.four = quizQuestions.options[3].text;
-        // optionsBtns.innerText = quizQuestions.options.text;
-        // optionsBtns.innerText = quizQuestions.options; THIS ONE COCATENATES OPTIONS
-        optionsBtnsOne.innerText = quizQuestions.options[0];
-        optionsBtnsTwo.innerText = quizQuestions.options[1];
-        optionsBtnsThree.innerText = quizQuestions.options[2];
-        optionsBtnsFour.innerText = quizQuestions.options[3];
+    optionOneEl.innerText = quizQuestions.option1;
+    optionTwoEl.innerText = quizQuestions.option2;
+    optionThreeEl.innerText = quizQuestions.option3;
+    optionFourEl.innerText = quizQuestions.option4;
 
-        // optionsBtns.innerText = quizQuestions.optionsSplit;
-        if (answer === quizQuestions.correctAnswer) {
-            response = "Correct!";
-            choiceResponseEl.textContent = response;
-        } else {
-            response = "Wrong!";
-            choiceResponseEl.textContent = response;
-        };
-    });
+    for (i = 0; i < optionsBtns.length; i++) { // find another way?
+        optionsBtns[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            userInput = Number(event.target.dataset.number);
+            checkAnswer(answer, userInput);
+            setTimeout(setNextQuestion, 500);
+        });
+    };
+    if (quizQuestions.length === currentQuestionIndex) {
+        winGame();
+    } else {
+        currentQuestionIndex++;
+    }
 };
 
-optionsBtns.addEventListener("click", (event) => {
-    event.preventDefault();
-    currentQuestionIndex++;
-    console.log(currentQuestionIndex);
-    setNextQuestion();
-});
+
+// document.querySelectorAll('.options').forEach(item => {
+//     item.addEventListener('click', event => {
+// handle click
+//     })
+//   })
 
 
-
-// subtractTime.addEventListener("click", function () {
-//   timer -= 10; // or
-// timer = timer + 10;
-// });
-
-
-// var loseGame = function() {
-// }
-
-
-
-quizQuestions = [{
-        question: "Arrays in JavaScript can be used to store?",
-        options: ["Numbers & Strings", "Booleans", "Other arrays", "All of the above"],
-        correctAnswer: "All of the above"
-    },
-    {
-        question: "Commonly used data types DO NOT include?",
-        options: ["Strings", "Booleans", "Alerts", "Numbers"],
-        correctAnswer: "Alerts"
-    },
-    {
-        question: "String values must be enclosed in?",
-        options: ["Single or double quotation marks", "Curly brackets", "Parenthesis", "Square brackets"],
-        correctAnswer: "Single or double quotation marks"
-    },
-    {
-        question: "Which of the following methods removes the last element from an array and returns that element?",
-        options: ["get()", "last()", "pop()", "None of the above"],
-        correctAnswer: "pop()"
-    },
-    {
-        question: "Which operator is used to assign a value to a variable?",
-        options: ["*", "X", "==", "="],
-        correctAnswer: "="
-    },
-    {
-        question: "Which of these is NOT a JavaScript comparison operator?",
-        options: ["===", "==!", ">=", "?"],
-        correctAnswer: "==!"
-    },
-    {
-        question: "JavaScript uses the + operator for both addition and concatenation.  Which of the following statements is TRUE?",
-        options: ["Numbers are concatenated. Strings are added.", "Numbers are added. Strings are concatenated.", "Both Numbers and Strings can be concatenated.", "Both Numbers and Strings can be added."],
-        correctAnswer: "Numbers are added. Strings are concatenated."
-    },
-    {
-        question: "Is the console a useful tool used in debugging of JavaScript?",
-        options: ["Yes, always", "More often than not", "Rarely", "Never"],
-        correctAnswer: "Yes, always"
-    },
-    {
-        question: "Which of the following String functions creates a string and displays it in a large font as if it were in a tag?",
-        options: ["anchor()", "blink()", "strong()", "big()"],
-        correctAnswer: "big()"
-    },
-    {
-        question: "How do you write an IF statement in JavaScript?",
-        options: ["if (i==5)", "if i=5 then", "if i==5 then", "if i=5"],
-        correctAnswer: "if (i==5)"
+function checkAnswer(answer, userInput) {
+    if (answer === userInput) {
+        response = "Correct!";
+        choiceResponseEl.classList.remove("hide");
+        choiceResponseEl.textContent = response;
+    } else {
+        response = "Wrong!  You lose 10 seconds.";
+        choiceResponseEl.classList.remove("hide");
+        choiceResponseEl.textContent = response;
+        subtractTime();
     }
-];
+};
+
+var subtractTime = function () {
+    // if (time > 10) {
+    //     time = time - 10;
+    //     timeEl.textContent = time;
+    // } else {
+    if (time <= 10) {
+        loseGame();
+        clearInterval(interval);
+        timeEl.textContent = "Time's up!";
+    }
+};
+
+var loseGame = function () {
+    questionsCardEl.classList.add("hide");
+    loserCardEl.classList.remove("hide");
+}
+
+var finalScoreEl;
+var winnerMsgEl;
+
+var winGame = function () {
+    saveInterval(interval);
+    questionsCardEl.classList.add("hide");
+    scoreCardEl.classList.remove("hide");
+    // capture the time/count & put into a var finalScoreEl
+    // winnerMsgEl.textContent = "You win!";
+    // enter name/initials
+}
 
 var init = function () {
     startBtn.addEventListener("click", startTimer);
--- Assets/js/scripths.js
+++ Assets/js/scripths.js
@@ -1,34 +1,68 @@
-var yourNameInput = document.querySelector("#your-name");
+var yourInitialsInput = document.querySelector("#your-initials");
 var saveBtn = document.querySelector("#save");
 var name;
 var score;
 
-var winnerSavedSpan = document.querySelector("#previous-winner");
+var nameSavedSpan = document.querySelector("#winner-name");
 var scoreSavedSpan = document.querySelector("#winner-score");
 
 
 
+// renderLastRegistered();
+
+// function renderLastRegistered() {
+//     var nameInput = localStorage.getItem("name");
+//     var highScore = localStorage.getItem("score");
+
+//     if (!initials || !score) {
+//         return;
+//     }
+
+//     winnerSavedSpan.textContent = initials;
+//     scoreSavedSpan.textContent = score;
+// }
 
 
-renderLastRegistered();
+// saveBtn.addEventListener("click", function (event) {
+//     event.preventDefault();
 
-function renderLastRegistered() {
-    var nameInput = localStorage.getItem("name");
-    var highScore = localStorage.getItem("score");
+//     localStorage.setItem("name", initials);
+//     localStorage.setItem("score", score);
+//     renderLastRegistered();
+// });
 
-    if (!initials || !score) {
-        return;
-    }
 
-    winnerSavedSpan.textContent = initials;
-    scoreSavedSpan.textContent = score;
-}
+var winnerEl = document.createElement("div");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+
+// for (var i = 0; i < listEl.length; i++) {
+//     console.log("Here are the all the previous winners: " + zooAnimals[i] + ".");
+// }
+
 
 
 saveBtn.addEventListener("click", function (event) {
     event.preventDefault();
 
-    localStorage.setItem("name", initials);
-    localStorage.setItem("score", score);
-    renderLastRegistered();
+    var winner = {
+        name: name.value,
+        score: score.value,
+    };
+
+    localStorage.setItem("winners", JSON.stringify(winner));
+    renderMessage();
+
 });
+
+function renderMessage() {
+    var previousWinners = JSON.parse(localStorage.getItem("winners"));
+    if (previousWinners !== null) {
+        document.querySelector(".message").textContent = previousWinners.name +
+            " : " + previousWinners.score
+    };
+}
